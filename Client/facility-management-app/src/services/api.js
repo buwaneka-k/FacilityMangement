88c712c927b00e7a9d4721f483e8401fcca23ecf
Buwaneka Kandekumbura <buwanekak@embla.asia>
2025-01-01 15:15:28 +0530

    conflicts

--- Client/facility-management-app/src/services/api.js
+++ Client/facility-management-app/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://localhost:7281/api/',
+  baseURL: 'https://api.example.com/',
   headers: {
     'Content-Type': 'application/json',
   },
